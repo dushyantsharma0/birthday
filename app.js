f5c4933cd334cf4b4559302ff20e968697d353fd
dushyantsharma0 <dushyantbhaman89@gmail.com>
2024-02-14 10:51:40 +0530

    spell

---
+++ app.js
@@ -0,0 +1,18 @@
+const express =require('express')
+const app=express()
+const port =process.env.PORT ||7000
+
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.get("/",(res,resp)=>{
+   const name= res.query.name
+   let alt=`http://localhost:7000/?name=${name}`
+   
+    resp.render("index",{name,alt})
+   
+
+})
+
+app.listen(port,()=>{
+    console.log(`app is running on ${port}this port`)
+})

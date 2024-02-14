2ed6785fe19a078297aafd008e4c78f54192aac8
dushyant <153808007+dushyantsharma0@users.noreply.github.com>
2024-02-14 12:15:16 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express =require('express')
 const app=express()
 const port =process.env.PORT ||7000
-
+const path = require('path');
 app.set('view engine', 'ejs')
 app.use(express.json())
 app.get('/',(res,resp)=>{
@@ -14,7 +14,7 @@ app.get("/bdy",(res,resp)=>{
    const name= res.query.name
    let alt=`http://localhost:7000/?name=${name}`
    
-    resp.render("index",{name,alt})
+    resp.render(`${path.join(__dirname,'views')}/index`,{name,alt})
    
 
 })
